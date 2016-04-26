f026f66bed76704206dc211ca3b5e99dde738fe7
kristincheverie <kcheverie@gmail.com>
2016-04-26 22:22:21 +0000

    add pagination

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,11 +2,11 @@ class Admin::UsersController < Admin::ApplicationController
 
   def index
     @users = User.all
+    @users = @users.page(params[:page]).per(1)
   end
 
   def show
     @user = User.find(params[:id])
   end
-
 
 end
