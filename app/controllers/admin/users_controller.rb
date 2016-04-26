022d7925dd2717001624c9f5856acc17c0afa951
kristincheverie <kcheverie@gmail.com>
2016-04-26 22:25:26 +0000

    added correct pagination

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def index
     @users = User.all
-    @users = @users.page(params[:page]).per(1)
+    @users = @users.page(params[:page]).per(10)
   end
 
   def show
