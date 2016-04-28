ea64e386f8381af87c2c52362e1a0eb7d99e8349
kristincheverie <kcheverie@gmail.com>
2016-04-28 15:23:44 +0000

    add create and delete to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,6 +23,20 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
